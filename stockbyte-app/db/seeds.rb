0f312362fcade473bc8a2dfa72e752dd44016471
haizhou85 <haizhou.guo@gmail.com>
2020-04-06 16:39:49 -0700

    add seeds

--- stockbyte-app/db/seeds.rb
+++ stockbyte-app/db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+test = User.first
+test.portfolios.create name: "default", total_worth:nil, total_cost:nil
+test_portfolio = test.portfolios.first
+stock =
+  {
+  symbol: "AAPL",
+  total_quantity: nil,
+  average_price: nil
+  }
+
+test_portfolio.stocks.create stock
+
+test_stock = test_portfolio.stocks.first
+
+trades = [
+  {
+    action:1,
+    price:250,
+    quantity:50,
+  },
+  {
+    action:1,
+    price:275,
+    quantity:20
+  }
+]
+
+trades.each do |attributes|
+  test_stock.trades.create attributes
+end
