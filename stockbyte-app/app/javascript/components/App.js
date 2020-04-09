ecd85a512272b3f9638c2e30e80b86567082208f
Ubuntu <ubuntu@ip-172-31-22-240.us-east-2.compute.internal>
2020-04-09 00:12:21 +0000

    added front page with tabels and a image at the top of the page

--- stockbyte-app/app/javascript/components/App.js
+++ stockbyte-app/app/javascript/components/App.js
@@ -1,37 +1,54 @@
 import React from "react"
 import PropTypes from "prop-types"
 import 'bootswatch/dist/litera/bootstrap.min.css'
-// import './application.css '
 import NavBar from './components/navigationbar'
-import Logg from './components/logg_table'
+import Cards from './components/cards'
+import image1 from './components/image1.png'
+import Flexbox from 'flexbox-react';
+import ReactRailsUJS from 'react_ujs'
+// import background from './components/background.jpg'
+// import './application.css';
+
 
 class App extends React.Component {
   render () {
-
-     
-
     const {
       logged_in,
       sign_in_route,
       sign_out_route
     } = this.props;
-
     return (
       
+ 
+     <div  className="container">
+    <React.Fragment> 
+     <img src= { image1 } alt="Card Icon" style={{width: "100%", height:"100%"}} />
+      <NavBar 
+          logged_in = { logged_in }
+          sign_in_route = { sign_in_route }
+          sign_out_route = { sign_out_route }/>
+      <Cards />         
+              <Flexbox flexDirection="column" minHeight="100vh">
 
-      
-      <React.Fragment>
-        <NavBar 
-        logged_in = { logged_in }
-        sign_in_route = { sign_in_route }
-        sign_out_route = { sign_out_route }
-      />
+              <Flexbox element="header" height="60px">
+                Header
+              </Flexbox>
+             
+              <Flexbox flexGrow={1}>
+                    
+              </Flexbox>
 
-
-      </React.Fragment>
-      
-    )
+              <Flexbox element="footer" height="60px">
+                Footer
+              </Flexbox>
+              
+              
+            </Flexbox>
+        
+    </React.Fragment>
+    </div>  
+    );
     }
 }
 
-export default App
+export default App;
