684d3fe7889f2344458e9d741ce321c0934e4756
Ubuntu <ubuntu@ip-172-31-37-50.ec2.internal>
2020-04-14 20:40:01 +0000

    Added links and changed more styling to about us

--- app/javascript/components/Pages/about.js
+++ app/javascript/components/Pages/about.js
@@ -2,7 +2,6 @@ import React from "react"
 import jeremy from './images/jeremy_photo.jpg'
 import linkedin from './images/linkedin.png'
 import github from './images/github.png'
-import Flexbox from 'flexbox-react';
 
 class About extends React.Component {
     render () {
@@ -12,34 +11,34 @@ class About extends React.Component {
             <div class="row" style={{marginLeft: "auto", marginRight: "auto"}}>
               <div class="col-sm-6">
                 <div class="card mb-5">
-                  <h3 class="card-header">Jeremy Lleva</h3>
+                  <h3 class="card-header">Richie Edquid</h3>
                   <div class="card-body">
-                    <h5 class="card-title">Application Title</h5>
+                    <h5 class="card-title">Position</h5>
                   </div>
                   <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto", borderRadius:"5px"}} src={jeremy} alt="Jeremy's photo for bio" />
                   <div class="card-body">
-                    <p class="card-text">Quick description of self</p>
+                    <p class="card-text">Quick Description</p>
                   </div>
                   <div class="card-body">
-                    <a href="#" class="card-link" > <img style={{height: "20px", width: "20px"}} src={linkedin} alt= "Linkedin" /> </a>
-                    <a href="#" class="card-link"> <img style={{height: "20px", width: "20px"}} src={github} alt= "Linkedin" /> </a>
+                    <a href="https://www.linkedin.com/in/richandcreamy/" class="card-link" > <img style={{height: "20px", width: "20px"}} src={linkedin} alt= "Linkedin" /> </a>
+                    <a href="https://github.com/richandcreamy" class="card-link"> <img style={{height: "20px", width: "20px"}} src={github} alt= "Linkedin" /> </a>
                   </div>
                 </div>
               </div>
               
               <div class="col-sm-6">
               <div class="card mb-5">
-                <h3 class="card-header">Card header</h3>
+                <h3 class="card-header">Haizhou Guo</h3>
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
+                  <h5 class="card-title">Position</h5>
                 </div>
-                <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto"}} src={jeremy} alt="Jeremy's photo for bio" />
+                <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto", borderRadius:"5px"}} src={jeremy} alt="Jeremy's photo for bio" />
                 <div class="card-body">
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
                   <div class="card-body">
-                    <a href="#" class="card-link" > <img style={{height: "20px", width: "20px"}} src={linkedin} alt= "Linkedin" /> </a>
-                    <a href="#" class="card-link"> <img style={{height: "20px", width: "20px"}} src={github} alt= "Linkedin" /> </a>
+                    <a href="https://www.linkedin.com/in/haizhou-guo/" class="card-link" > <img style={{height: "20px", width: "20px"}} src={linkedin} alt= "Linkedin" /> </a>
+                    <a href="https://github.com/haizhou85" class="card-link"> <img style={{height: "20px", width: "20px"}} src={github} alt= "Linkedin" /> </a>
                   </div>
               </div>
               </div>
@@ -48,34 +47,34 @@ class About extends React.Component {
             <div class="row" style={{marginLeft: "auto", marginRight: "auto"}}>
               <div class="col-sm-6" >
               <div class="card mb-5">
-                <h3 class="card-header">Card header</h3>
+                <h3 class="card-header">Jeremy Lleva</h3>
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
+                  <h5 class="card-title">Backend Developer</h5>
                 </div>
-                <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto"}} src={jeremy} alt="Jeremy's photo for bio" />
+                <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto", borderRadius:"5px"}} src={jeremy} alt="Jeremy's photo for bio" />
                 <div class="card-body">
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
                   <div class="card-body">
-                    <a href="#" class="card-link" > <img style={{height: "20px", width: "20px"}} src={linkedin} alt= "Linkedin" /> </a>
-                    <a href="#" class="card-link"> <img style={{height: "20px", width: "20px"}} src={github} alt= "Linkedin" /> </a>
+                    <a href="https://www.linkedin.com/in/jeremy-lleva/" class="card-link" > <img style={{height: "20px", width: "20px"}} src={linkedin} alt= "Linkedin" /> </a>
+                    <a href="https://github.com/JeremyLleva" class="card-link"> <img style={{height: "20px", width: "20px"}} src={github} alt= "Linkedin" /> </a>
                   </div>
               </div>
               </div>
               
               <div class="col-sm-6">
               <div class="card mb-5">
-                <h3 class="card-header">Card header</h3>
+                <h3 class="card-header">Xena Sitnikova</h3>
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
+                  <h5 class="card-title">Position</h5>
                 </div>
-                <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto"}} src={jeremy} alt="Jeremy's photo for bio" />
+                <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto", borderRadius:"5px"}} src={jeremy} alt="Jeremy's photo for bio" />
                 <div class="card-body">
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
                   <div class="card-body">
-                    <a href="#" class="card-link" > <img style={{height: "20px", width: "20px"}} src={linkedin} alt= "Linkedin" /> </a>
-                    <a href="#" class="card-link"> <img style={{height: "20px", width: "20px"}} src={github} alt= "Linkedin" /> </a>
+                    <a href="https://www.linkedin.com/in/sxena/" class="card-link" > <img style={{height: "20px", width: "20px"}} src={linkedin} alt= "Linkedin" /> </a>
+                    <a href="https://github.com/sxena" class="card-link"> <img style={{height: "20px", width: "20px"}} src={github} alt= "Linkedin" /> </a>
                   </div>
               </div>
               </div>
