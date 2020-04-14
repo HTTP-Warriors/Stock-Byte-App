adcda4efc25bb8a127bbd625191b8603c206f5fa
Ubuntu <ubuntu@ip-172-31-37-50.ec2.internal>
2020-04-14 18:16:09 +0000

    More styling on about us added rows and columns

--- app/javascript/components/Pages/about.js
+++ app/javascript/components/Pages/about.js
@@ -1,36 +1,90 @@
 import React from "react"
 import jeremy from './images/jeremy_photo.jpg'
+import Flexbox from 'flexbox-react';
+
 class About extends React.Component {
     render () {
-
+    const aboutus = {
+        marginLeft: "auto", 
+        marginRight: "auto",
+        display:Flexbox.flex,
+        flexWrap: Flexbox.wrap
+    }
 
     return (
-        <React.Fragment>
+        <div class="aboutus">
             <h1> Meet our Devs </h1>
-            <div class="card mb-3">
-              <h3 class="card-header">Card header</h3>
-              <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+            <div class="row">
+              <div class="col-sm-6">
+              <div class="card mb-3">
+                <h3 class="card-header">Card header</h3>
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                </div>
+                <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto"}} src={jeremy} alt="Jeremy's photo for bio" />
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card-body">
+                  <a href="#" class="card-link">Card link</a>
+                  <a href="#" class="card-link">Another link</a>
+                </div>
               </div>
-              <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto"}} src={jeremy} alt="Jeremy's photo for bio" />
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Vestibulum at eros</li>
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+              
+              <div class="col-sm-6">
+              <div class="card mb-3">
+                <h3 class="card-header">Card header</h3>
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                </div>
+                <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto"}} src={jeremy} alt="Jeremy's photo for bio" />
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card-body">
+                  <a href="#" class="card-link">Card link</a>
+                  <a href="#" class="card-link">Another link</a>
+                </div>
               </div>
-              <div class="card-footer text-muted">
-                2 days ago
               </div>
             </div>
-        </React.Fragment>
+            <div class="row">
+              <div class="col-sm-6">
+              <div class="card mb-3">
+                <h3 class="card-header">Card header</h3>
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                </div>
+                <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto"}} src={jeremy} alt="Jeremy's photo for bio" />
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card-body">
+                  <a href="#" class="card-link">Card link</a>
+                  <a href="#" class="card-link">Another link</a>
+                </div>
+              </div>
+              </div>
+              
+              <div class="col-sm-6">
+              <div class="card mb-3">
+                <h3 class="card-header">Card header</h3>
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                </div>
+                <img style={{height:"200px", display:"block", marginLeft: "auto", marginRight: "auto"}} src={jeremy} alt="Jeremy's photo for bio" />
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card-body">
+                  <a href="#" class="card-link">Card link</a>
+                  <a href="#" class="card-link">Another link</a>
+                </div>
+              </div>
+              </div>
+            </div>
+        </div>
         );
     }
 }
