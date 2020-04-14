811ffe7605c34016e35b645faafc1f1f0a446f11
Ubuntu <ubuntu@ip-172-31-44-221.us-east-2.compute.internal>
2020-04-14 23:14:10 +0000

    dont merge this branchgit add .!

--- app/javascript/components/components/navigationbar.js
+++ app/javascript/components/components/navigationbar.js
@@ -44,7 +44,7 @@ class NavBar extends React.Component {
       <React.Fragment>
 
 
-    <nav class="navbar navbar-inverse" >
+    <nav class="navbar navbar-inverse">
       <div class="container-fluid">
 
         </div>
