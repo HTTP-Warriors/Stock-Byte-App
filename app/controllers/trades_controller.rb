37e20e83a213aee1577adea5d2acf9961eb74cfb
Ubuntu <ubuntu@ip-172-31-37-50.ec2.internal>
2020-04-14 21:54:56 +0000

    Cleaned up code and commenting for controllers (portfolios, stocks, and trades)

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,4 +1,6 @@
 class TradesController < ApplicationController
+    
+    # Grabs a list of trades based on current user and stock
     # GET http://localhost:3000/trades?portfolio=default&stock=AAPL
     def index
         #Assigns Users if there is no current user
@@ -13,6 +15,8 @@ class TradesController < ApplicationController
             render json: []
         end
     end
+    
+    # Creates a trade based on stock and portfolio information
     # POST http://localhost:3000/trades?portfolio=default&stock=AAPL, trade_params
     def create
         #Assigns user if there is no current user
@@ -22,11 +26,11 @@ class TradesController < ApplicationController
             @trade = @stock.trades.create(trade_params)
             render json: @trade
         else
-            # @user = User.first
-            # @portfolio = @user.portfolios.find_by(name: params[:portfolio])
             render json: []
         end
     end
+    
+    # Deletes a trade based on id, stock, and portfolio
     # DELETE http://localhost:3000/trades/#{id}?portfolio=default&stock=AAPL
     def destroy
       if current_user
@@ -34,15 +38,13 @@ class TradesController < ApplicationController
         @stock = @portfolio.stocks.find_by(symbol: params[:stock])
         @trade = @stock.trades.find(params[:id])
         @trade.destroy
-      # else
-        # @user = User.first
-        # @portfolio = @user.portfolios.find_by(name: params[:portfolio])
       end
     end
 
-
-private
-    def trade_params
-        params.require(:trade).permit(:action, :price, :quantity)
-    end
+    private
+        
+        # Parameters needed for creating a trade
+        def trade_params
+            params.require(:trade).permit(:action, :price, :quantity)
+        end
 end
