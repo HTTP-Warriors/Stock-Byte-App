d7141a4ce982e49a60ce21da6b9b534cb2d692af
haizhou85 <haizhou.guo@gmail.com>
2020-04-15 23:21:08 -0700

    net worth can display properly, add validation in handleSubmit, only valid symbol can be added to stocks table. add bootstrap columns

--- app/javascript/components/Pages/playground.js
+++ app/javascript/components/Pages/playground.js
@@ -1,10 +1,11 @@
 import React from "react"
 
+
 class Playground extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      networth: 0,
+      netWorth: 100000,
       unrealizedGain: 0,
       hasPlaygroundAccount: false,
       playgroundAccountData: [],
@@ -15,15 +16,13 @@ class Playground extends React.Component {
           symbol: ""
       },
       tradeForm: {
-        quantity:"",
-        action: "1"
+        quantity:""
       }
     }
+
   }
   componentDidMount(){
       this.getPortfolio()
-      this.getStockList()
-      this.updatePortfolioValue()
   }
 
   getPortfolio = () => {
@@ -36,11 +35,11 @@ class Playground extends React.Component {
     )
     .then((result) => {
         if(result.length > 1){
-          console.log(result)
           this.setState({
             playgroundAccountData: result[1],
             hasPlaygroundAccount: true
           })
+          this.getStockList()
         }
     })
   }
@@ -67,12 +66,13 @@ class Playground extends React.Component {
         }
       })
       .then((result)=>{
-        this.setState({stockList: result})
+        this.setState({ stockList: result })
         result.map((stock)=>{
-          this.getCurrentPrice(stock.symbol)
+        this.getCurrentPrice(stock.symbol)
         })
       })
     }
+
   getCurrentPrice = (symbol) => {
     fetch(`https://api.twelvedata.com/price?symbol=${symbol}&apikey=bc07ae0baa6241d79c88764a862a7dba`)
       .then((response)=>{
@@ -99,32 +99,48 @@ class Playground extends React.Component {
     })
     .then((response) => {
         if(response.ok){
-            return this.getStockList()
+          return this.getStockList()
         }
     })
   }
 
+  validSymbol = (symbol) => {
+    fetch(`https://api.twelvedata.com/price?symbol=${symbol}&apikey=bc07ae0baa6241d79c88764a862a7dba`)
+      .then((response)=>{
+        if(response.status === 200){
+           return(response.json())
+         }
+      })
+      .then((result)=>{
+        console.log(result);
+        if(result.price){
+          return true
+        }else{
+          return false
+        }
+      })
+  }
+
   handleSubmit = (event) => {
     event.preventDefault()
-    const { form, stockList } = this.state
-    let inPortfolio = false
-    stockList.map((stock)=>{
-      if(stock.symbol===form.symbol){
-        inPortfolio = true
-      }
-    })
-    if(inPortfolio){
-      this.setState({
-        stockInFocus: form.symbol
+    const { form } = this.state
+    fetch(`https://api.twelvedata.com/price?symbol=${form.symbol}&apikey=bc07ae0baa6241d79c88764a862a7dba`)
+      .then((response)=>{
+        if(response.status === 200){
+           return(response.json())
+         }
       })
-    }else{
-      this.createStock(form.symbol)
-      this.setState({
-      stockInFocus: form.symbol
+      .then((result)=>{
+        if(result.price){
+          this.createStock(form.symbol)
+          this.setState({
+            stockInFocus: form.symbol
+          })
+        }
       })
-    }
 
   }
+
 
   handleChange = (event) => {
       let { form } = this.state
@@ -135,16 +151,15 @@ class Playground extends React.Component {
   }
 
   validTrade = (tradeForm) => {
-    let action = tradeForm.action
-    let quantity = tradeForm.quantity
     this.getStockList()
     const { playgroundAccountData, stockList } = this.state
+    let action = tradeForm.action
+    let quantity = tradeForm.quantity
     let symbol = this.state.stockInFocus
     let currentPrice = this.state.currentPrices[`${ symbol }`]
     let cash = playgroundAccountData.cash
     let value = currentPrice * quantity * action
     let position = stockList.find(value => value.symbol === symbol).total_quantity
-    console.log(cash,value);
     if(action == 1 && cash >= value){
       return { value: value, tradeForm: { action: 1, quantity: quantity, price: currentPrice } }
     }else if(action == -1 && quantity <= position){
@@ -156,16 +171,8 @@ class Playground extends React.Component {
 
 
   createTrade = (request) => {
-    // let action = tradeForm.action
-    // this.getStockList()
-    // const { stockInFocus, playgroundAccountData } = this.state
     let symbol = this.state.stockInFocus
-    // let price = this.state.currentPrices[`${ symbol }`]
-    // tradeForm.price = price
-    // let value = price * tradeForm.quantity * tradeForm.action
-    // let cash = playgroundAccountData.cash
     let validForm = this.validTrade(request)
-    console.log(validForm);
     if(validForm.tradeForm){
         return fetch(`/trades?portfolio=playground&stock=${symbol}`, {
           body: JSON.stringify(validForm.tradeForm),
@@ -177,18 +184,25 @@ class Playground extends React.Component {
           .then((response) => {
             if(response.ok){
               this.updatePortfolio(validForm.value)
-              this.updatePortfolioValue()
               this.getStockList()
             }
           })
-      }else{
+        }else{
         alert("something is wrong, cannot place the trade")
       }
   }
 
 
-  handleTradeSubmit = (event) => {
+  handleBuySubmit = (event) => {
     event.preventDefault()
+    let { tradeForm } = this.state
+    tradeForm.action = 1
+    this.createTrade(this.state.tradeForm)
+  }
+  handleSellSubmit = (event) => {
+    event.preventDefault()
+    let { tradeForm } = this.state
+    tradeForm.action = -1
     this.createTrade(this.state.tradeForm)
   }
   // store what user entered to form
@@ -201,12 +215,10 @@ class Playground extends React.Component {
   updatePortfolio = (value) => {
     const { playgroundAccountData } = this.state
     let cash = playgroundAccountData.cash
-    // let name = playgroundAccountData.name
     let id = playgroundAccountData.id
     let portfolioParams = {
       cash: cash - value
     }
-    console.log(portfolioParams)
     fetch(`/portfolios/${id}`,
     {
       method: 'PUT',
@@ -218,19 +230,15 @@ class Playground extends React.Component {
         return this.getPortfolio()
       })
   }
-  updatePortfolioValue = () => {
-    let { playgroundAccountData ,networth, unrealizedGain, stockList, currentPrices} =this.state
-    networth = playgroundAccountData.cash
-    stockList.map(value => networth += value.total_quantity * currentPrices[`${ value.symbol }`])
-    unrealizedGain = networth - (playgroundAccountData.total_cost + playgroundAccountData.cash)
-    console.log(networth, unrealizedGain, playgroundAccountData.cash)
-    this.setState({networth: networth, unrealizedGain: unrealizedGain})
-  }
-
-
 
   render(){
-    let { playgroundAccountData, stockList, currentPrices, networth, unrealizedGain } = this.state
+    const { playgroundAccountData, stockList, currentPrices } = this.state
+    let netWorth = playgroundAccountData.cash
+    let unrealizedGain = 0
+    if(stockList.length > 0){
+      stockList.map(stock => netWorth += stock.total_quantity * currentPrices[`${ stock.symbol }`])
+      unrealizedGain = netWorth - (playgroundAccountData.total_cost + playgroundAccountData.cash)
+    }
     return(
       <>
         <h1>Playground</h1>
@@ -245,56 +253,71 @@ class Playground extends React.Component {
         }
         { this.state.hasPlaygroundAccount &&
           <div>
-            <h4>Cash: ${ playgroundAccountData.cash }</h4>
-            <h4>Networth: ${ networth }</h4>
-            <h4>Unrealized Gain/Loss : ${ unrealizedGain }</h4>
-            
+          <div class="row">
+            <div class="col-sm-3">
+              <div>
+                <h4>Cash: ${ playgroundAccountData.cash.toFixed(2) }</h4>
+                <h4>Net Worth: ${ netWorth.toFixed(2) }</h4>
+                <h4>Total Gain/Loss: ${ (netWorth-100000).toFixed(2) }</h4>
+                <h4>Unrealized Gain/Loss : ${ unrealizedGain.toFixed(2) }</h4>
+              </div>
+
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">Symbol</th>
+                    <th scope="col">Average Price</th>
+                    <th scope="col">Position</th>
+                    <th scope="col">Current Price</th>
+                    <th scope="col">Gain/Loss</th>
+                  </tr>
+                </thead>
+                <tbody>
+                { (stockList?stockList.filter(stock => stock.total_quantity>0):[]).map((stock, index) => {
+                  return(
+                    <tr class="table-dark" key={ index }>
+                      <th scope="row">
+                        <a href={`/stock/${ stock.symbol }`}>{ stock.symbol }</a>
+                        <br />
+                      </th>
+                      <td>{ stock.average_price.toFixed(2) }</td>
+                      <td>{ stock.total_quantity }</td>
+                      <td>{ currentPrices[`${ stock.symbol }`]}</td>
+                      <td>{ (currentPrices[`${ stock.symbol }`] * stock.total_quantity - stock.value).toFixed(2) }</td>
+                    </tr>)}
+                  )
+                }
+                </tbody>
+              </table>
+
+            </div>
+            <div class="col-sm-6">
+              <h1>{ this.state.stockInFocus }</h1>
+            </div>
+            <div class="col-sm-3">.col-sm-3</div>
+          </div>
+
+
+
+
+
             <div class="form-group">
                 <label class="col-form-label" for="inputDefault">Find a stock</label>
                 <input onChange={ this.handleChange } type="text" class="form-control" name="symbol" Placeholder="Enter Stock Symbol Here"/>
                 <button type="submit" onClick= { this.handleSubmit }>Find</button>
             </div>
 
-            <table class="table table-hover">
-              <thead>
-                <tr>
-                  <th scope="col">Symbol</th>
-                  <th scope="col">Average Price</th>
-                  <th scope="col">Quantity</th>
-                  <th scope="col">Current Price</th>
-                  <th scope="col">Gain/Loss</th>
-                </tr>
-              </thead>
-              <tbody>
-              { (stockList?stockList.filter(stock => stock.total_quantity>0):[]).map((stock, index) => {
-                return(
-                  <tr class="table-dark" key={ index }>
-                    <th scope="row">
-                      <a href={`/stock/${ stock.symbol }`}>{ stock.symbol }</a>
-                      <br />
-                    </th>
-                    <td>{ stock.average_price.toFixed(2) }</td>
-                    <td>{ stock.total_quantity }</td>
-                    <td>{ currentPrices[`${ stock.symbol }`]}</td>
-                    <td>{ (currentPrices[`${ stock.symbol }`] * stock.total_quantity - stock.value).toFixed(2) }</td>
-                  </tr>)}
-                )
-              }
-              </tbody>
-            </table>
 
             <form>
               <div class="form-group">
-                <label >Buying or Selling</label>
-                <select onChange={ this.handleTradeChange } type="text" value = { this.state.tradeForm.action } class="form-control" name="action">
-                  <option value="1">Buy</option>
-                  <option value="-1">Sell</option>
-                </select>
                 <label class="col-form-label" for="inputDefault">Quantity of stocks</label>
                 <input onChange={ this.handleTradeChange } type="text" class="form-control" name="quantity"/>
-                <button type="submit" onClick= { this.handleTradeSubmit }>Trade</button>
+                <button type="submit" onClick= { this.handleBuySubmit } >Buy</button>
+                <button type="submit" onClick= { this.handleSellSubmit } >Sell</button>
               </div>
             </form>
+
+
 
           </div>
         }
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,23 +9,23 @@ class Stock < ApplicationRecord
         end
         @sum
     end
-    
+
     def value
         @sum = 0
         trades.each do |trade|
             @sum += trade.cost
         end
-        @sum.round(2)
+        @sum
     end
-    
+
     def average_price
         if total_quantity == 0
             return 0
         else
-            (value / total_quantity).round(2)
+            (value / total_quantity)
         end
     end
-    
+
     # Redfine json method to also include cost, value, and average price in json render
     def as_json(options = {})
       super options.merge(methods: [:total_quantity, :value, :average_price])
